cc534937ad7e66a6147843d3d41d4ec8e6787c61
i5ting <i5ting@126.com>
2016-12-15 14:38:40 +0800

    lazycommmit, please ignore

--- app.js
+++ app.js
@@ -13,29 +13,52 @@ const MyBigView = require('./MyBigView')
 const MyPagelet = require('./MyPagelet')
 
 app.get('/', function (req, res) {
-  var bigpipe = new MyBigView(req, res, 'index', { title: "测试" })
+  var bigpipe = new MyBigView(req, res, 'basic/index', { title: "测试" })
 
   var pagelet1 = new MyPagelet('pagelet1', { is: "pagelet1测试" })
 
   pagelet1.location = 'pagelet1'
   pagelet1.root = 'views'
-  pagelet1.tpl = 'p1.html'
+  pagelet1.tpl = 'basic/p1.html'
   pagelet1.selector = 'pagelet1'
   pagelet1.delay = 2000
 
-  var pagelet2 = new MyPagelet('pagelet2', 'p', { t: "测试" })
+  var pagelet2 = new MyPagelet('pagelet2', 'basic/p', { t: "测试" })
   pagelet2.selector = 'pagelet2'
   pagelet2.location = 'pagelet2'
   pagelet2.root = 'views'
-  pagelet2.tpl = 'p2.html'
+  pagelet2.tpl = 'basic/p2.html'
   pagelet2.delay = 4000
 
-  pagelet1.addChild(pagelet2)
   bigpipe.add(pagelet1)
-  // bigpipe.add(pagelet2)
+  bigpipe.add(pagelet2)
+
+  bigpipe.start()
+});
+
+app.get('/nest', function (req, res) {
+  var bigpipe = new MyBigView(req, res, 'nest/index', { title: "测试" })
+
+  var pagelet1 = new MyPagelet('pagelet1', { is: "pagelet1测试" })
+
+  pagelet1.location = 'pagelet1'
+  pagelet1.root = 'views'
+  pagelet1.tpl = 'nest/p1.html'
+  pagelet1.selector = 'pagelet1'
+  pagelet1.delay = 2000
+
+  var subPagelet = new MyPagelet('pagelet2', 'nest/p', { t: "测试" })
+  subPagelet.selector = 'pagelet2'
+  subPagelet.location = 'pagelet2'
+  subPagelet.root = 'views'
+  subPagelet.tpl = 'nest/p2.html'
+  subPagelet.delay = 4000
+
+  pagelet1.addChild(subPagelet)
+
+  bigpipe.add(pagelet1)
 
   bigpipe.start()
 });
 
 app.listen(5000);
-
