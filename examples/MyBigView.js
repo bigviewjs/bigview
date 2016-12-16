5deea1ca04985270b19da48db66e3ce38c98e149
i5ting <i5ting@126.com>
2016-12-16 10:39:40 +0800

    lazycommmit, please ignore

---
+++ examples/MyBigView.js
@@ -0,0 +1,15 @@
+const BigView = require('../packages/bigview')
+
+module.exports = class MyBigView extends BigView {
+  before () {
+     return new Promise(function(resolve, reject) {
+        setTimeout(function(){
+          resolve(true)
+        }, 0)
+    })
+  }
+
+  // after () {
+  //
+  // }
+}
---
+++ examples/bin.js
@@ -0,0 +1,6 @@
+var app = require('./app')
+
+app.listen(4005)
+
+var open = require("open");
+open("http://127.0.0.1:4005");
