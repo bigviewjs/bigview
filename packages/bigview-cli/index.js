833c8315baa67e13193b80c4681b5a65a3a0784f
i5ting <i5ting@126.com>
2016-12-20 18:05:25 +0800

    refact, please ignore

--- packages/bigview-cli/index.js
+++ packages/bigview-cli/index.js
@@ -10,27 +10,24 @@ var current_path = process.cwd();
 var tpl_apply = require('tpl_apply')
 
 for (var i = 1; i < argv.length; i++) {
-  console.log(argv[i])
+  // console.log(argv[i])
   var moduleName = argv[i]
   generatePageletModule (moduleName) 
 }
 
 //
-
-
 function generatePageletModule (moduleName) {
-  var files = fs.readdirSync("./tpl/pagelet")
-  console.log(files)
+  var files = fs.readdirSync(file_path + "/tpl/pagelet")
+  // console.log(files)
   for(var i in files){
     var file = files[i]
     gOne(file, moduleName)
   }
-  
 }
 
 function gOne(tpl, moduleName) {
   if (/^\./.test(tpl)) return
-  console.log(tpl)
+  // console.log(tpl)
   var mkdirp = require('mkdirp');
 
   var source = file_path + "/tpl/pagelet/" + tpl
@@ -39,12 +36,11 @@ function gOne(tpl, moduleName) {
   
   mkdirp(destDir, function (err) {
       if (err) console.error(err)
-      else console.log('pow!')
+      else console.log('generate ' + dest)
         
       tpl_apply.tpl_apply(source, {
         tpl: tpl,
         name: moduleName
       }, dest);
-  });
-  
+  }); 
 }
