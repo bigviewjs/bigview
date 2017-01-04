bdaebe62b848e14d02c7c2e41f39d7d414baee48
i5ting <i5ting@126.com>
2017-01-04 12:58:02 +0800

    refact, please ignore

--- packages/bigview/mode/render.js
+++ packages/bigview/mode/render.js
@@ -3,6 +3,7 @@
 // 情况2： 随机，all完成之后，立即写入，即parallel模式
 // 情况3： 依次，写入
 // 情况4： 依次，不写入，all完成之后再写入(当前)
+// 情况5： 一次渲染模式：即普通模式，不写入布局，所有pagelet执行完成，一次写入到浏览器。支持搜索引擎，用来支持那些不支持JS的客户端。
 module.exports = class RenderMode {
   constructor() {
     this.isLayoutWriteImmediately = false
