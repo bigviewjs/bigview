d8c63e27f8c648e17945fc87812cffe28d6bae68
i5ting <i5ting@126.com>
2017-01-06 15:50:55 +0800

    refact, please ignore

--- packages/bigview/mode/render.js
+++ packages/bigview/mode/render.js
@@ -1,10 +1,14 @@
 'use strict'
-// pagelets 4种情况
+
+const debug = require('debug')('bigview');
+
+// pagelets 5种情况
 // 情况1： 随机，先完成的先写入，即pipeline模式
 // 情况2： 随机，all完成之后，立即写入，即parallel模式
-// 情况3： 依次，写入
-// 情况4： 依次，不写入，all完成之后再写入(当前)
-// 情况5： 一次渲染模式：即普通模式，不写入布局，所有pagelet执行完成，一次写入到浏览器。支持搜索引擎，用来支持那些不支持JS的客户端。
+// 情况3： 连续模式reduce：依次连续写入
+// 情况4： 即连续渲染模式reducerender，不写入布局，所有pagelet顺序执行完成，一次写入到浏览器。
+// 情况5： 一次渲染模式render：即普通模式，不写入布局，所有pagelet执行完成，一次写入到浏览器。支持搜索引擎，用来支持那些不支持JS的客户端。(当前)
+
 module.exports = class RenderMode {
 	constructor() {
 		this.isLayoutWriteImmediately = false
