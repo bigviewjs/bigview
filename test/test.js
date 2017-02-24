3db6d492529a9ba72879b14fde9f1015cc8de910
shilong.sang <i5ting@126.com>
2017-02-24 17:30:50 +0800

    refact, please ignore

--- test/test.js
+++ test/test.js
@@ -1,26 +1,10 @@
-// import test from 'ava'
-//
-// const app = require('../examples/app')
-//
-// const testChunks = require('testchunks')
-//
-// test.cb('GET /', t => {
-//   testChunks(app, '/',function(chunks, error, response, body){
-//     t.is(chunks.length, 3)
-//     t.end()
-//   })
-// })
-//
-// test.cb('GET /nest', t => {
-//   testChunks(app, '/nest',function(chunks, error, response, body){
-//     t.is(chunks.length, 3)
-//     t.end()
-//   })
-// })
-//
-// test.cb('GET /nest2', t => {
-//   testChunks(app, '/nest2',function(chunks, error, response, body){
-//     t.is(chunks.length, 3)
-//     t.end()
-//   })
-// })
+import test from 'ava'
+
+const app = require('../examples/app')
+
+
+test('GET /', t => {
+  t.pass()
+})
+
+
