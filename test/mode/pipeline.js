27c203c933e02d0f28c9ddd6b95cd9e0fbb8da42
shilong.sang <i5ting@126.com>
2017-02-27 17:16:35 +0800

    lazycommmit, please ignore

---
+++ test/mode/pipeline.js
@@ -0,0 +1,44 @@
+import test from 'ava'
+// 初始化bigview
+// 增加biglet1
+// 增加biglet2
+// 增加biglet3
+// 异步输出
+
+const Bigview = require("../../packages/bigview")
+const Biglet = require("../../packages/biglet")
+
+module.exports = class MyPagelet extends Pagelet {
+  constructor () {
+      super()
+      this.root = __dirname
+      this.name = 'pagelet1'
+      this.data = { 
+          is: "pagelet1测试",
+          po: {
+              name: this.name
+          }
+      }
+      this.domid = 'pagelet1'
+      this.location = 'pagelet1'
+      // this.tpl = 'tpl/p1.html'
+      this.delay = 4000
+  }
+  
+  fetch() {
+    return this.sleep(this.delay)
+  }
+  
+  sleep(time) {
+    return new Promise((resolve)=> setTimeout(resolve, time))
+  }
+
+}
+
+
+test('GET /', t => {
+    console.log(Bigview)
+  t.pass()
+})
+
+
