57256e8a5173a40406864d83977438492b64e32d
shilong.sang <i5ting@126.com>
2017-03-06 20:23:28 +0800

    lazycommmit, please ignore

--- test/mode/pipeline.js
+++ test/mode/pipeline.js
@@ -1,44 +1,9 @@
 import test from 'ava'
 
 const sinon = require('sinon')
-// 初始化bigview
-// 增加biglet1
-// 增加biglet2
-// 增加biglet3
-// 异步输出
-
 const Bigview = require("../../packages/bigview")
 const Biglet = require("../../packages/biglet")
-
-// module.exports = class MyPagelet extends Pagelet {
-//   constructor () {
-//       super()
-//       this.root = __dirname
-//       this.name = 'pagelet1'
-//       this.data = { 
-//           is: "pagelet1测试",
-//           po: {
-//               name: this.name
-//           }
-//       }
-//       this.domid = 'pagelet1'
-//       this.location = 'pagelet1'
-//       // this.tpl = 'tpl/p1.html'
-//       this.delay = 4000
-//   }
-  
-//   fetch() {
-//     return this.sleep(this.delay)
-//   }
-  
-//   sleep(time) {
-//     return new Promise((resolve)=> setTimeout(resolve, time))
-//   }
-
-// }
 const ModeInstanceMappings = require('../../packages/bigview/mode')
-
-const Utils = require('../../packages/bigview/utils')
 
 test.cb('GET /', t => {
     let req = {}
