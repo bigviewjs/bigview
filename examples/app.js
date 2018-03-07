2cae58b2a6a9040cd343f3549ab6ebfb2a777330
codezyc <zyc199144@gmail.com>
2018-03-07 20:52:18 +0800

    填坑

--- examples/app.js
+++ examples/app.js
@@ -1,45 +1,44 @@
-'use strict';
+'use strict'
 
-var express = require('express');
-var path = require('path');
-var ejs = require('ejs');
-var cookieParser = require('cookie-parser');
+var express = require('express')
+var path = require('path')
+var ejs = require('ejs')
+var cookieParser = require('cookie-parser')
 
-var app = express();
+var app = express()
 
-app.use(cookieParser());
+app.use(cookieParser())
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.engine('.html', ejs.__express);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')))
+app.engine('.html', ejs.__express)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'html')
 
-app.get('/', require('./bpmodules/basic'));
+app.get('/', require('./bpmodules/basic'))
 
-app.get('/payload', require('./bpmodules/payload'));
+app.get('/payload', require('./bpmodules/payload'))
 
-app.get('/data', require('./bpmodules/dataStore'));
+app.get('/data', require('./bpmodules/dataStore'))
 
-app.get('/if', require('./bpmodules/if'));
+app.get('/if', require('./bpmodules/if'))
 
-app.get('/error', require('./bpmodules/error'));
+app.get('/error', require('./bpmodules/error'))
 
-app.get('/nest',require('./bpmodules/nest'));
+app.get('/nest', require('./bpmodules/nest'))
 
-app.get('/nest2', require('./bpmodules/nest2'));
+app.get('/nest2', require('./bpmodules/nest2'))
 
 app.get('/default', function (req, res) {
   var pagelets = []
-  
+
   pagelets.push(require('./bpmodules/basic/p1'))
   pagelets.push(require('./bpmodules/basic/p2'))
-    
+
   res.render('basic/default', {
-    title: "default test",
+    title: 'default test',
     pagelets: pagelets
   })
-});
-
+})
 
 // app.listen(5000);
 module.exports = app
--- examples/bpmodules/basic/p1/index.js
+++ examples/bpmodules/basic/p1/index.js
@@ -4,27 +4,26 @@ const Pagelet = require('../../../../packages/biglet')
 
 module.exports = class MyPagelet extends Pagelet {
   constructor () {
-      super()
-      this.root = __dirname
-      this.name = 'pagelet1'
-      this.data = { 
-          is: "pagelet1测试",
-          po: {
-              name: this.name
-          }
+    super()
+    this.root = __dirname
+    this.name = 'pagelet1'
+    this.data = {
+      is: 'pagelet1测试',
+      po: {
+        name: this.name
       }
-      this.domid = 'pagelet1'
-      this.location = 'pagelet1'
+    }
+    this.domid = 'pagelet1'
+    this.location = 'pagelet1'
       // this.tpl = 'tpl/p1.html'
-      this.delay = 4000
-  }
-  
-  fetch() {
-    return this.sleep(this.delay)
-  }
-  
-  sleep(time) {
-    return new Promise((resolve)=> setTimeout(resolve, time))
+    this.delay = 4000
   }
 
+  fetch () {
+    return this.sleep(this.delay)
+  }
+
+  sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time))
+  }
 }
